2deb46b51db363a3f5d5b9c038597d0361df3bf3
Reid Blomquist <Reid.Blomquist@nike.com>
2017-04-26 20:39:03 -0700

    the colors duke, the colors

--- src/drawinator.js
+++ src/drawinator.js
@@ -8,13 +8,23 @@ ns.drawSomeStuff = () => {
   var paths = [];
 
   paths[0] = 'M -100 -100 L 200 100 L 170 200 L 100 100 z';
-  paths[1] = 'M -100 -100 L 200 100 L -100 200 L 100 100 z';
+  paths[1] = 'M -100 -100 L 200 100 L 40 200 L 100 100 z';
 
   paths.forEach((p) => {
     var paf = new fabric.Path(p);
-    paf.set({ left: 120, top: 120 });
+    paf.set({ left: 120, top: 120, fill: getRandomColor() });
     canvas.add(paf)
   });
+
+  // Generate some random colors while we play
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 
   // Make sure we always have a full size canvas, cuz why not?!
   window.addEventListener('resize', resizeCanvas, false);
