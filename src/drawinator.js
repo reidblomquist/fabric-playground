251efb5b881a759c578714244fe116daf1279f93
Reid Blomquist <Reid.Blomquist@nike.com>
2017-04-26 20:32:13 -0700

    gettin' pathy with it

--- src/drawinator.js
+++ src/drawinator.js
@@ -5,17 +5,28 @@ let ns = {};
 ns.drawSomeStuff = () => {
   var canvas = new fabric.Canvas('c');
 
-  // create a rectangle object
-  var rect = new fabric.Rect({
-    left: 100,
-    top: 100,
-    fill: 'red',
-    width: 20,
-    height: 20
+  var paths = [];
+
+  paths[0] = 'M -100 -100 L 200 100 L 170 200 L 100 100 z';
+  paths[1] = 'M -100 -100 L 200 100 L -100 200 L 100 100 z';
+
+  paths.forEach((p) => {
+    var paf = new fabric.Path(p);
+    paf.set({ left: 120, top: 120 });
+    canvas.add(paf)
   });
 
-  // "add" rectangle onto canvas
-  canvas.add(rect);
+  // Make sure we always have a full size canvas, cuz why not?!
+  window.addEventListener('resize', resizeCanvas, false);
+
+  function resizeCanvas() {
+    canvas.setHeight(window.innerHeight);
+    canvas.setWidth(window.innerWidth);
+    canvas.renderAll();
+  }
+
+  // resize on init
+  resizeCanvas();
 }
 
 export default ns;
